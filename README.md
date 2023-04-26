a60bd4fc976b60925dbbda3ea7d6afdf4f1c6235
Bhpsngum <bhpsngumtrongwikipediatiengvie@gmail.com>
2023-04-27 08:11:51 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Arena-mod-remake
-Remake of Arena mod
+Remake of Arena mod (aka Arena Mod Recontinuation)
+
+## What's this?
+This is the remake of the old Arena Mod, which its development has been stopped by their developers
+
+## What's in here?
+There are the `files` folders, which contains basically disassembled parts of the mod
+In the root directory, you can also see other files:
+* **`README.md`**: This file
+* **`mergeFiles.js`**: This file is used to assemble the codes to create a complete mod file, only run in NodeJS
+* **(whatever its name)**: The compiled file, output of `mergeFile.js`
---
+++ mergeFiles.js
@@ -0,0 +1,23 @@
+// NodeJS program to merge Arena files into one runnable code
+
+const fs = require("fs").promises;
+const template_file = "arena_base_template.js";
+const destination_file_name = "Arena_mod_v4.0_compiled.js";
+
+const getContent = async function (fileName) {
+    let content = await fs.readFile("./files/" + fileName, "utf-8");
+    let match;
+    while ((match = content.match(/\/\*\simport\s(.+?)\s\*\//)) != null) {
+        let index = match.index;
+        let len = match[0].length;
+        let file = match[1];
+        content = content.slice(0, index) + `\n\n/* Imported from ${file} at ${new Date().toString()} */\n\n` + await getContent(file) + content.slice(index + len, content.length);
+    }
+    return content;
+}
+
+getContent(template_file).then(content => {
+    fs.writeFile(destination_file_name, content)
+    .then(e => console.log("Write successfully to", destination_file_name))
+    .catch(e => console.error("Failed to write to file. Caught Error:", e));
+}).catch(e => console.error("Failed to parse content. Caught Error:", e));
