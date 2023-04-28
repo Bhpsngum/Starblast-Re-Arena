9524c0f88240773aaf7ab169c320a4bbd03cffbf
Bhpsngum <bhpsngumtrongwikipediatiengvie@gmail.com>
2023-04-28 17:44:26 +0900

    lmao

--- files/Maps.js
+++ files/Maps.js
@@ -112,7 +112,7 @@ const Maps = [
     },
     {
         name: "Flames",
-        author: "Leon",
+        author: "Noel",
         spawnpoints: [
             { x: 390, y: -390},
             { x: -390, y: 390 }
@@ -221,7 +221,7 @@ const Maps = [
     },
     {
         name: "Nebula",
-        author: "Leon",
+        author: "Noel",
         spawnpoints: [
             { x: 390, y: -390 },
             { x: -390, y: 390 }
@@ -330,7 +330,7 @@ const Maps = [
     },
     {
         name: "Blockchain",
-        author: "Leon",
+        author: "Noel",
         spawnpoints: [
             { x: 390, y: 0},
             { x: -390, y: 0}
