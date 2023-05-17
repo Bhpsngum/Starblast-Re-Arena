96f24371f87eeb0e71aa5df274cff41db79e4d9b
Bhpsngum <bhpsngumtrongwikipediatiengvie@gmail.com>
2023-05-17 23:12:54 +0900

    small fix

--- Arena_Mod_v4.0.0_ShipTesting.js
+++ Arena_Mod_v4.0.0_ShipTesting.js
@@ -90,7 +90,7 @@ you can fck around and find out how to compile custom templates as well
 
 
 
-/* Imported from Config_ShipTesting.js at Wed May 17 2023 22:22:20 GMT+0900 (Japan Standard Time) */
+/* Imported from Config_ShipTesting.js at Wed May 17 2023 23:12:36 GMT+0900 (Japan Standard Time) */
 
 const DEBUG = true; // if in debug phase
 
@@ -133,7 +133,7 @@ GAME_OPTIONS.max_players = Math.trunc(Math.min(Math.max(GAME_OPTIONS.max_players
 
 
 
-/* Imported from Teams.js at Wed May 17 2023 22:22:20 GMT+0900 (Japan Standard Time) */
+/* Imported from Teams.js at Wed May 17 2023 23:12:36 GMT+0900 (Japan Standard Time) */
 
 const Teams = [
     {
@@ -183,7 +183,7 @@ const GhostTeam = {
 
 
 
-/* Imported from Maps.js at Wed May 17 2023 22:22:20 GMT+0900 (Japan Standard Time) */
+/* Imported from Maps.js at Wed May 17 2023 23:12:36 GMT+0900 (Japan Standard Time) */
 
 const Maps = [
     {
@@ -1780,7 +1780,7 @@ const Maps = [
 
 
 
-/* Imported from Abilities.js at Wed May 17 2023 22:22:20 GMT+0900 (Japan Standard Time) */
+/* Imported from Abilities.js at Wed May 17 2023 23:12:36 GMT+0900 (Japan Standard Time) */
 
 const ShipAbilities = {
     "Test ship": {
@@ -3774,7 +3774,7 @@ const ShipAbilities = {
 
 
 
-/* Imported from Commands.js at Wed May 17 2023 22:22:20 GMT+0900 (Japan Standard Time) */
+/* Imported from Commands.js at Wed May 17 2023 23:12:36 GMT+0900 (Japan Standard Time) */
 
 // only available when DEBUG is `true`
 const MAKE_COMMANDS = function () {
@@ -4013,7 +4013,7 @@ const MAKE_COMMANDS = function () {
 
 
 
-/* Imported from Resources.js at Wed May 17 2023 22:22:20 GMT+0900 (Japan Standard Time) */
+/* Imported from Resources.js at Wed May 17 2023 23:12:36 GMT+0900 (Japan Standard Time) */
 
 const RESOURCES = {
     planeOBJ: "https://starblast.data.neuronality.com/mods/objects/plane.obj"
@@ -4023,7 +4023,7 @@ const RESOURCES = {
 
 
 
-/* Imported from HelperFunctions.js at Wed May 17 2023 22:22:20 GMT+0900 (Japan Standard Time) */
+/* Imported from HelperFunctions.js at Wed May 17 2023 23:12:36 GMT+0900 (Japan Standard Time) */
 
 const HelperFunctions = {
     toHSLA: function (hue = 0, alpha = 1, saturation = 100, lightness = 50) {
@@ -4374,7 +4374,7 @@ const HelperFunctions = {
 
 
 
-/* Imported from Managers.js at Wed May 17 2023 22:22:20 GMT+0900 (Japan Standard Time) */
+/* Imported from Managers.js at Wed May 17 2023 23:12:36 GMT+0900 (Japan Standard Time) */
 
 const TeamManager = {
     ghostTeam: GhostTeam,
@@ -4759,9 +4759,7 @@ const AbilityManager = {
         ship.set({
             type: shipAbil.codes.default,
             generator: shipAbil.generatorInit,
-            stats: AbilityManager.maxStats,
-            vx: 0,
-            vy: 0
+            stats: AbilityManager.maxStats
         });
         shipAbil.initialize(ship);
         shipAbil.unload(ship);
