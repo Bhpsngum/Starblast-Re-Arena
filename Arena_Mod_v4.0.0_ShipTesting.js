0e0f4dff82542947cbd47beff2ee1a8ee65b30f1
Bhpsngum <bhpsngumtrongwikipediatiengvie@gmail.com>
2023-05-18 11:39:12 +0900

    fixed: being pucked while pucking won't set yourself back to puck after your pucking phase ends

--- Arena_Mod_v4.0.0_ShipTesting.js
+++ Arena_Mod_v4.0.0_ShipTesting.js
@@ -90,7 +90,7 @@ you can fck around and find out how to compile custom templates as well
 
 
 
-/* Imported from Config_ShipTesting.js at Thu May 18 2023 04:11:31 GMT+0900 (Japan Standard Time) */
+/* Imported from Config_ShipTesting.js at Thu May 18 2023 11:35:06 GMT+0900 (Japan Standard Time) */
 
 const DEBUG = true; // if in debug phase
 
@@ -133,7 +133,7 @@ GAME_OPTIONS.max_players = Math.trunc(Math.min(Math.max(GAME_OPTIONS.max_players
 
 
 
-/* Imported from Teams.js at Thu May 18 2023 04:11:31 GMT+0900 (Japan Standard Time) */
+/* Imported from Teams.js at Thu May 18 2023 11:35:06 GMT+0900 (Japan Standard Time) */
 
 const Teams = [
     {
@@ -183,7 +183,7 @@ const GhostTeam = {
 
 
 
-/* Imported from Maps.js at Thu May 18 2023 04:11:31 GMT+0900 (Japan Standard Time) */
+/* Imported from Maps.js at Thu May 18 2023 11:35:06 GMT+0900 (Japan Standard Time) */
 
 const Maps = [
     {
@@ -1780,7 +1780,7 @@ const Maps = [
 
 
 
-/* Imported from Abilities.js at Thu May 18 2023 04:11:31 GMT+0900 (Japan Standard Time) */
+/* Imported from Abilities.js at Thu May 18 2023 11:35:06 GMT+0900 (Japan Standard Time) */
 
 const ShipAbilities = {
     "Test ship": {
@@ -2934,8 +2934,6 @@ const ShipAbilities = {
             if (player.custom.inAbility) AbilityManager.end(player);
             player.set({type: this.codes.ability, stats: AbilityManager.maxStats});
 
-            if (player.custom.abilityCustom != null) player.custom.abilityCustom.puckTriggered = null;
-
             if (player.custom.pucked != null) HelperFunctions.TimeManager.clearTimeout(player.custom.pucked);
             
             player.custom.pucked = HelperFunctions.TimeManager.setTimeout(function () {
@@ -3773,7 +3771,7 @@ const ShipAbilities = {
 
 
 
-/* Imported from Commands.js at Thu May 18 2023 04:11:31 GMT+0900 (Japan Standard Time) */
+/* Imported from Commands.js at Thu May 18 2023 11:35:06 GMT+0900 (Japan Standard Time) */
 
 // only available when DEBUG is `true`
 const MAKE_COMMANDS = function () {
@@ -4012,7 +4010,7 @@ const MAKE_COMMANDS = function () {
 
 
 
-/* Imported from Resources.js at Thu May 18 2023 04:11:31 GMT+0900 (Japan Standard Time) */
+/* Imported from Resources.js at Thu May 18 2023 11:35:06 GMT+0900 (Japan Standard Time) */
 
 const RESOURCES = {
     planeOBJ: "https://starblast.data.neuronality.com/mods/objects/plane.obj"
@@ -4022,7 +4020,7 @@ const RESOURCES = {
 
 
 
-/* Imported from HelperFunctions.js at Thu May 18 2023 04:11:31 GMT+0900 (Japan Standard Time) */
+/* Imported from HelperFunctions.js at Thu May 18 2023 11:35:06 GMT+0900 (Japan Standard Time) */
 
 const HelperFunctions = {
     toHSLA: function (hue = 0, alpha = 1, saturation = 100, lightness = 50) {
@@ -4373,7 +4371,7 @@ const HelperFunctions = {
 
 
 
-/* Imported from Managers.js at Thu May 18 2023 04:11:31 GMT+0900 (Japan Standard Time) */
+/* Imported from Managers.js at Thu May 18 2023 11:35:06 GMT+0900 (Japan Standard Time) */
 
 const TeamManager = {
     ghostTeam: GhostTeam,
